6ec9d960333c2d12f7380152e95794fce8f8af3b
will sherman <willsherman771@gmail.com>
2023-12-07 19:00:21 -0500

    to  deploy

--- viteportfolio/src/main.jsx
+++ viteportfolio/src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '/src/index.css'
+
+import App from './app';
+import Landing from './pages/Landing.jsx';
+import TheMan from './pages/TheMan.jsx';
+import TheWork from './pages/TheWork.jsx';
+import TheCall from './pages/TheCall.jsx';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <Landing />,
+            },
+            {
+                path: '/man',
+                element: <TheMan />,
+            },
+            {
+                path: '/work',
+                element: <TheWork />,
+            },
+            {
+                path: '/call',
+                element: <TheCall />,
+            },
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+);
