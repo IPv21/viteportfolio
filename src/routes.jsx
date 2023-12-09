0f6271a2199122c7a774a11d5a59f57f831cca2e
will sherman <willsherman771@gmail.com>
2023-12-09 18:10:30 -0500

    llll

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import App from './App.jsx';
 import Landing from './components/Landing.jsx';
@@ -10,14 +10,16 @@ import TheCall from './components/TheCall.jsx';
 const Routes = () => {
   return (
     <Router basename={import.meta.env.BASE_URL}>
-      <Switch>
-        <Route path="/" exact element={<App />}>
-          <Route index element={<Landing />} />
-          <Route path="/man" element={<TheMan />} />
-          <Route path="/work" element={<TheWork />} />
-          <Route path="/call" element={<TheCall />} />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route
+          path="/"
+          element={<App />}
+          index={<Route element={<Landing />} />}
+        />
+        <Route path="/man" element={<TheMan />} />
+        <Route path="/work" element={<TheWork />} />
+        <Route path="/call" element={<TheCall />} />
+      </Routes>
     </Router>
   );
 };
